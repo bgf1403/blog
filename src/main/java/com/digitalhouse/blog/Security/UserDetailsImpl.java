de18d50b30071c83ed3a971708d57affb6fdbf14
Bruno Grassi Ferreira <brunoferreira03@globo.com>
2021-10-20 16:11:20 -0300

    Implementado o Service

--- src/main/java/com/digitalhouse/blog/Security/UserDetailsImpl.java
+++ src/main/java/com/digitalhouse/blog/Security/UserDetailsImpl.java
@@ -1,11 +1,25 @@
 package com.digitalhouse.blog.Security;
 
+import com.digitalhouse.blog.Model.Usuario;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 
 public class UserDetailsImpl implements UserDetails {
+
+    private static final long serialVersionUID = 1L;
+    private String username;
+    private String password;
+
+    public UserDetailsImpl() {
+    }
+
+    public UserDetailsImpl(Usuario user) {
+        this.username = user.getUsuario();
+        this.password = user.getSenha();
+    }
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return null;
@@ -40,4 +54,6 @@ public class UserDetailsImpl implements UserDetails {
     public boolean isEnabled() {
         return true;
     }
+
+
 }
--- src/main/java/com/digitalhouse/blog/Security/UserServiceDetailsImpl.java
+++ src/main/java/com/digitalhouse/blog/Security/UserServiceDetailsImpl.java
@@ -1,7 +1,27 @@
 package com.digitalhouse.blog.Security;
 
+import com.digitalhouse.blog.Model.Usuario;
+import com.digitalhouse.blog.Repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
-public class UserServiceDetailsImpl {
+public class UserServiceDetailsImpl implements UserDetailsService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username){
+        Optional<Usuario> user = usuarioRepository.findByUsuario(username);
+        user.orElseThrow(() -> new UsernameNotFoundException(username + " not found."));
+
+        return user.map(UserDetailsImpl::new).get();
+    }
+
 }
